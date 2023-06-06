6f5b566873d74fccb27b4a7edf6209e44f3cfce7
fcb1987 <dzidapati@interia.pl>
2023-06-06 17:46:56 +0200

    dodanie funkcji kalkulatora

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,8 +2,22 @@ package org.example;
 
 public class Calculator {
 
-    public int add(int x, int y){
-        return x + y;
-
+    public int add(int a,int b) {
+        return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int multi(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add( 4, 7));
+        System.out.println(calculator.add()
 
     }
 }
